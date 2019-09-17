7c22bd15fc4c72651e64cf6f551f8ca9c7ffb685
albrecht99 <mateogiulliano@hotmail.com>
2019-09-17 14:43:17 -0400

    Se agrega servicio que oculta barra de estado y botones

--- lib/Inicio.dart
+++ lib/Inicio.dart
@@ -4,12 +4,17 @@ import 'package:flutter/services.dart';
 class Inicio extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //--------------------------------------------------
     // Servicio que bloquea la orientacion vertical
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.landscapeLeft,
       DeviceOrientation.landscapeRight,
     ]);
-
+    //---------------------------------------------------
+    // Servico que oculta todo y deja pantalla completa
+    List<SystemUiOverlay> sys = [];
+    SystemChrome.setEnabledSystemUIOverlays(sys);
+    //---------------------------------------------------
     return MaterialApp(
       title: "¿Mba'e?",
       debugShowCheckedModeBanner: false,
