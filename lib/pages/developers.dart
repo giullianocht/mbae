68069f85d360469895677d17fc62085fe0123275
albrecht99 <mateogiulliano@hotmail.com>
2019-09-17 23:21:45 -0400

    Orden

--- lib/pages/developers.dart
+++ lib/pages/developers.dart
@@ -4,28 +4,28 @@ class DevelopersInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightGreen,
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("¿ Mba'e ?"),
         backgroundColor: Colors.lightGreen,
-      ),
-        body: Center(
-      child: Container(
-        color: Colors.transparent,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Probando",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 40.0,
-              ),
-            ),
-          ],
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("¿ Mba'e ?"),
+          backgroundColor: Colors.lightGreen,
         ),
-      ),
-    ));
+        body: Center(
+          child: Container(
+            color: Colors.transparent,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Probando",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
