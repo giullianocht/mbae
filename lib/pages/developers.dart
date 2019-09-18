11c930c543e909b32371546dfdc1523a9a3e2539
albrecht99 <mateogiulliano@hotmail.com>
2019-09-17 23:17:53 -0400

    Se agrega estilo a la pag info

--- lib/pages/developers.dart
+++ lib/pages/developers.dart
@@ -4,22 +4,28 @@ class DevelopersInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
+      backgroundColor: Colors.lightGreen,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("¿ Mba'e ?"),
+        backgroundColor: Colors.lightGreen,
+      ),
         body: Center(
-          child: Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  "HOLA TEAM",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 40.0,
-                  ),
-                ),
-              ],
+      child: Container(
+        color: Colors.transparent,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Probando",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 40.0,
+              ),
             ),
-          ),
-        ));
+          ],
+        ),
+      ),
+    ));
   }
 }
