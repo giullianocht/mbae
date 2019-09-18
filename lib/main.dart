50bfb29cff8d075ce0a8db33fac4988829235eae
Cristhian_Ortellado <breideworld@gmail.com>
2019-09-18 17:07:57 -0400

    probando

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,73 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+void main() {
+  SystemChrome.setEnabledSystemUIOverlays([]);
+  runApp(new MaterialApp(
+    home: Mypage(),
+  ));
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Mypage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MypageState createState() => new _MypageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _MypageState extends State<Mypage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+    return new Scaffold(
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: PageView(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            FadeInImage(
+              placeholder: AssetImage('imagenes/vidrio.png'),
+              image: AssetImage('imagenes/prueba1.jpg'),
+              fit: BoxFit.fill,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            FadeInImage(
+              placeholder: AssetImage('imagenes/vidrio.png'),
+              image: AssetImage('imagenes/instruccion2.png'),
+              fit: BoxFit.fill,
             ),
+            FadeInImage(
+              placeholder: AssetImage('imagenes/vidrio.png'),
+              image: AssetImage('imagenes/instruccion3.jpg'),
+              fit: BoxFit.fill,
+            ),
+            Container(
+                decoration:BoxDecoration(
+                  image:DecorationImage( 
+                  image:AssetImage('imagenes/animado.gif'),
+                  fit: BoxFit.cover
+                  ),
+                ),
+                padding: EdgeInsets.only(top: 370),
+                child: RaisedButton(
+                          //Vuelve al ultimo activity
+                          onPressed: () {
+                            _salir();
+                          },
+                          child: Text(
+                            "Salir",
+                          ),  
+                          color: Colors.indigo,
+                          textColor: Colors.white,
+                          splashColor: Colors.green,
+                        ),
+              ),
+
           ],
+          scrollDirection: Axis.horizontal,
+          pageSnapping: true,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _salir() {
+    Navigator.pop(context);
   }
 }
---
+++ lib/pageview.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(MaterialApp(
+    title:"Androidmonks",
+    home: Scaffold(
+      body: PageView(),
+    ),
+  ));
+}
