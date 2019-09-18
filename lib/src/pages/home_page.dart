9b34a83f44589fcb3f4165f55ace32ee58ed46a2
mgueyraud <mgueyraud.junior@gmail.com>
2019-09-18 15:44:07 -0400

    Acelerometro

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -3,17 +3,34 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 import 'package:mbae/src/providers/tarjeta_provider.dart';
+import 'package:sensors/sensors.dart';
 
 //Pagina Home
 class HomePage extends StatefulWidget{
+
+  
   
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+
+   // event returned from accelerometer stream
+  AccelerometerEvent event;
+  // hold a refernce to these, so that they can be disposed
+  StreamSubscription accel;
+  Timer timerAccelerometer;
+
+  // positions and count
+  int count = 0;
+
+  // Timer para los 60 segundos
   Timer _timer;
   int _start = 60;
+
+  //Color
+  Color background = Colors.yellow;
 
   void startTimer(int start) {
 
@@ -37,14 +54,15 @@ class _HomePageState extends State<HomePage> {
 
 
   _HomePageState(){
-    startTimer(10);
+    startTimer(60);
+    startAccelerometer();
   }
 
   @override
   Widget build(BuildContext context) {
     
     return Scaffold(
-      backgroundColor: Colors.yellow,
+      backgroundColor: background,
       body: Center(
         child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -61,6 +79,67 @@ class _HomePageState extends State<HomePage> {
 
   }
 
+  startAccelerometer() {
+    // if the accelerometer subscription hasn't been created, go ahead and create it
+    if (accel == null) {
+      accel = accelerometerEvents.listen((AccelerometerEvent eve) {
+        setState(() {
+          event = eve;
+        });
+      });
+    } else {
+      // it has already ben created so just resume it
+      accel.resume();
+    }
+
+    // Accelerometer events come faster than we need them so a timer is used to only proccess them every 200 milliseconds
+    if (timerAccelerometer == null || !timerAccelerometer.isActive) {
+      timerAccelerometer = Timer.periodic(Duration(milliseconds: 200), (_) {
+        // if count has increased greater than 3 call pause timer to handle success
+        if (count > 3) {
+          pauseTimer();
+        } else {
+          // proccess the current event
+          setColor(event);
+        }
+      });
+    }
+
+
+  }
+
+  setColor(AccelerometerEvent event){
+    // Calculate Left
+    double z = event.z ;
+
+    if(z >= 7) {
+      background = Colors.lightGreenAccent;
+    }else if(z <= -7){
+      background = Colors.redAccent;
+    }else{
+      background = Colors.yellow;      
+    }
+
+    setState(() {});
+  }
+
+  pauseTimer() {
+    // stop the timer and pause the accelerometer stream
+    timerAccelerometer.cancel();
+    accel.pause();
+
+    // set the success color and reset the count
+    setState(() {
+      count = 0;
+    });
+  }
+
+  @override
+  void dispose() {
+    timerAccelerometer?.cancel();
+    accel?.cancel();
+    super.dispose();
+  }
   
 
 // Widget _prueba() {
