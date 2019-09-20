f9487c0af54abbcc082ea6f7025213dee9b210f2
Cristhian_Ortellado <breideworld@gmail.com>
2019-09-20 10:13:20 -0400

    Cambio de fondo y boton del ultimo PageView Instrucciones

--- lib/src/pages/instrucciones_page.dart
+++ lib/src/pages/instrucciones_page.dart
@@ -12,6 +12,7 @@ class _InstruccionesState extends State<Instrucciones> {
       body: Center(
         child: PageView(
           children: <Widget>[
+            //Se genera una imagen por pagina
             FadeInImage(
               placeholder: AssetImage('assets/vidrio.png'),
               image: AssetImage('assets/prueba1.jpg'),
@@ -27,23 +28,42 @@ class _InstruccionesState extends State<Instrucciones> {
               image: AssetImage('assets/instruccion3.jpg'),
               fit: BoxFit.fill,
             ),
+            //PARA LA ULTIMA PAGINA SE INSERTA UNA IMAGEN Y UN BOTON DE SALIDA
             Container(
+              
                 decoration:BoxDecoration(
                   image:DecorationImage( 
-                  image:AssetImage('assets/animado.gif'),
+                  image:AssetImage('assets/inicio.jpg'),
                   fit: BoxFit.cover
                   ),
                 ),
-                padding: EdgeInsets.only(top: 370),
+                //padding:MainAxisAlignment.end,
+                 padding:EdgeInsets.only(
+                  top: 350,
+                  left: 220,
+                  right: 220,
+
+                  ),
+                
                 child: RaisedButton(
+                  
                           //Vuelve al ultimo activity -
                           onPressed: () {
                             _salir();
                           },
                           child: Text(
-                            "Salir",
+                            "Volver al Menu Principal",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              backgroundColor: Color(900),
+                              fontStyle: FontStyle.italic,
+                              fontFamily: "HeyAugust",
+
+                              
+                            ),
                           ),  
-                          color: Colors.indigo,
+                          color:Colors.redAccent,
                           textColor: Colors.white,
                           splashColor: Colors.green,
                         ),
