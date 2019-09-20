e1584ec87685e930b12800420517b878380c4e72
albrecht99 <mateogiulliano@hotmail.com>
2019-09-20 00:09:38 -0400

    Se corrigen los espacios

--- lib/src/widgets/contact_developer.dart
+++ lib/src/widgets/contact_developer.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/gestures.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 TextStyle defaultStyle = TextStyle(fontSize: 17);
