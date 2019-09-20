75a960ec1dbe98ddf42093cb7283b05de60f53f9
albrecht99 <mateogiulliano@hotmail.com>
2019-09-19 23:55:27 -0400

    Se Agrega mejoras a la pagina de presentacion

--- lib/src/pages/developers_page.dart
+++ lib/src/pages/developers_page.dart
@@ -1,31 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:mbae/src/widgets/contact_developer.dart';
+
+TextStyle defaultStyle = TextStyle(fontSize: 17);
 
 class DevelopersInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.lightGreen,
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text("¿ Mba'e ?"),
-          backgroundColor: Colors.lightGreen,
-        ),
-        body: Center(
-          child: Container(
-            color: Colors.transparent,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  "Probando",
+      backgroundColor: Colors.lightGreen,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.home, size: 25.0, color: Colors.white),
+        elevation: 50.0,
+        backgroundColor: Colors.transparent,
+        onPressed: () {
+          print("Boton Flotante presionado");
+          Navigator.pop(context);
+        },
+      ),
+      body: Container(
+        color: Colors.transparent,
+        child: Column(
+          children: <Widget>[
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Text(
+                  "¿ Mba'e ?",
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 40.0,
+                    fontSize: 20.0,
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
-        ));
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 60.0),
+              child: Container(
+                child: RichText(
+                  textAlign: TextAlign.justify,
+                  text: TextSpan(
+                    style: defaultStyle,
+                    children: <TextSpan>[
+                      TextSpan(
+                        text:
+                            "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15.0),
+              child: Center(
+                child: Text(
+                  "Desarrolladores",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
+            ContactDeveloper("Giulliano Albrecht",
+                "https://www.instagram.com/giulliano_cht"),
+            ContactDeveloper("Cristhian Ortellado",
+                "https://www.instagram.com/giulliano_cht"),
+            ContactDeveloper(
+                "Mario Gueraund", "https://www.instagram.com/giulliano_cht"),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/src/widgets/contact_developer.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+TextStyle defaultStyle = TextStyle(fontSize: 17);
+
+class ContactDeveloper extends StatelessWidget {
+  final String _nombre;
+  final String _pathinstagram;
+
+  ContactDeveloper(this._nombre, this._pathinstagram);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(children: <Widget>[
+      Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 3.0),
+          child: Container(
+            child: Row(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    RichText(
+                      textAlign: TextAlign.left,
+                      text: TextSpan(
+                        style: defaultStyle,
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: "- " + this._nombre,
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                launch(_pathinstagram);
+                              },
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Icon(
+                    Icons.assignment_ind,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ))
+    ]));
+  }
+}
