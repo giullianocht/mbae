1169f3b97443a1e8b625d7a5b147482e9efcd8fc
albrecht99 <mateogiulliano@hotmail.com>
2019-10-12 20:06:29 -0300

    Se aplica formateo

--- lib/src/pages/instrucciones_page.dart
+++ lib/src/pages/instrucciones_page.dart
@@ -7,14 +7,12 @@ class Instrucciones extends StatefulWidget {
 }
 
 class _InstruccionesState extends State<Instrucciones> {
-
   double height, width;
   int paginaActual = 0;
 
-  PageController pagina =PageController();
+  PageController pagina = PageController();
   @override
   Widget build(BuildContext context) {
-
     height = MediaQuery.of(context).size.height;
     width = MediaQuery.of(context).size.width;
 
@@ -31,79 +29,83 @@ class _InstruccionesState extends State<Instrucciones> {
       backgroundColor: Colors.cyan,
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[ 
-            Container(
-              height: porcentaje(80, height),
-              child: PageView(
-                onPageChanged: (pagina){
-                  paginaActual = pagina;
-                  setState(() {});
-                },
-              scrollDirection: Axis.horizontal,
-              pageSnapping: true,
-              controller: pagina,
-              
-              children: <Widget>[
-                //Se genera una imagen por pagina
-                Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Image.asset('assets/instrucciones/gesto_correcto.png', width: porcentaje(30, width),),
-                      Text('Una vez arriba es correcto'),
-                    ],
-                  ),
-                ),
-                 Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Image.asset('assets/instrucciones/gesto_ayuda.png', width: porcentaje(30, width),),
-                      Text('Una vez abajo es ayuda')
-                    ],
-                  ),
-                ),
-                 Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Image.asset('assets/instrucciones/gesto_incorrecto.png', width: porcentaje(30, width),),
-                      Text('Una vez abajo es incorrecto')
-                    ],
-                  ),
-                ),
-                //PARA LA ULTIMA PAGINA SE INSERTA UNA IMAGEN Y UN BOTON DE SALIDA
-                Center(
-                  child: Container(
-                      child: Text("JAHUGA"),
-                  ),
-                ),
-                
-              ],
-              
-              
-          ),
-            ),
-            Container(
-              child: DotsIndicator(
-                dotsCount: 4,
-                position: paginaActual,
-                decorator: DotsDecorator(
-                  activeColor: Colors.redAccent,
-                  // size: const Size.square(9.0),
-                  // activeSize: const Size(18.0, 9.0),
-                  // activeShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0.0)),
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Container(
+                height: porcentaje(80, height),
+                child: PageView(
+                  onPageChanged: (pagina) {
+                    paginaActual = pagina;
+                    setState(() {});
+                  },
+                  scrollDirection: Axis.horizontal,
+                  pageSnapping: true,
+                  controller: pagina,
+                  children: <Widget>[
+                    //Se genera una imagen por pagina
+                    Container(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Image.asset(
+                            'assets/instrucciones/gesto_correcto.png',
+                            width: porcentaje(30, width),
+                          ),
+                          Text('Una vez arriba es correcto'),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Image.asset(
+                            'assets/instrucciones/gesto_ayuda.png',
+                            width: porcentaje(30, width),
+                          ),
+                          Text('Una vez abajo es ayuda')
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Image.asset(
+                            'assets/instrucciones/gesto_incorrecto.png',
+                            width: porcentaje(30, width),
+                          ),
+                          Text('Una vez abajo es incorrecto')
+                        ],
+                      ),
+                    ),
+                    //PARA LA ULTIMA PAGINA SE INSERTA UNA IMAGEN Y UN BOTON DE SALIDA
+                    Center(
+                      child: Container(
+                        child: Text("JAHUGA"),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            )
-          ]
-        ),
+              Container(
+                child: DotsIndicator(
+                  dotsCount: 4,
+                  position: paginaActual,
+                  decorator: DotsDecorator(
+                    activeColor: Colors.redAccent,
+                    // size: const Size.square(9.0),
+                    // activeSize: const Size(18.0, 9.0),
+                    // activeShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0.0)),
+                  ),
+                ),
+              )
+            ]),
       ),
     );
   }
-  
-  double porcentaje(double porcentaje, double valor){
-    return (porcentaje*valor) / 100;
+
+  double porcentaje(double porcentaje, double valor) {
+    return (porcentaje * valor) / 100;
   }
 }
